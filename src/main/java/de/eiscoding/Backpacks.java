f99f3c133158ef91beb960d4b0d510e8e3b2cf37
EISCoding <zockergeilo@gmail.com>
2025-10-29 08:55:15 +0100

    gpt

--- src/main/java/de/eiscoding/Backpacks.java
+++ src/main/java/de/eiscoding/Backpacks.java
@@ -1,7 +1,11 @@
 package de.eiscoding;
 
+import de.eiscoding.content.BackpackItem;
 import de.eiscoding.registry.BackpackRegistries;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,4 +23,12 @@ public class Backpacks implements ModInitializer {
     public static Identifier id(String path) {
         return Identifier.of(MOD_ID, path);
     }
+
+    public static final Item BACKPACK = Registry.register(
+            Registries.ITEM,
+            new Identifier(MOD_ID, "backpack"),
+            new BackpackItem(new Item.Settings()
+                    .armor(net.minecraft.item.equipment.ArmorMaterials.LEATHER,
+                            net.minecraft.item.equipment.EquipmentType.CHESTPLATE))
+    );
 }
